c15905346d23a0dda0d56ff2969ebddd1ba52456
Andy Hutton <shandy_hutton@hotmail.com>
2023-01-26 12:58:52 +0000

    added spacing to boxes

--- app.js
+++ app.js
@@ -14,34 +14,35 @@ if (user == "andy") {
 
 let learn = confirm("Would you like to  learn about me?");
 if (learn == true) {
-  alert("hey " + user + " lets learn about me.");
+  alert("Hey " + user + " lets learn about me.");
 } else {
   alert("why dont you want to know about me.");
 }
 
-let uni = confirm("do you think i went to university.");
+let uni = confirm("Do you think i went to university.");
 if (uni == false) {
-  alert("I started working straight after my A levels.");
-} else {
-  alert("I should have! But I started working.");
+  alert("Correct I didn't go, I started working straight after my A levels.");
   Points++;
+} else {
+  alert("Wrong I didn't go! I started working in a bar.");
 }
 
 let wife = confirm(user + " Do you think im married?");
 if (wife == true) {
-  alert("I am, we have been married for six years.");
+  alert("Correct, we have been married for six years.");
+  Points++;
 } else {
   alert(
-    "the single life wasnt for me, I always wanted to be married and have children!"
+    "wrong, the single life wasnt for me... I always wanted to be married and have children!"
   );
-  Points++;
 }
 
 let children = confirm("Do you think I have children");
 if (children == true) {
-  alert("I have two amazing boys");
+  alert("Correct I have two amazing boys");
+  Points++;
 } else {
-  alert(user + "I would get a lot more sleep if I didnt!");
+  alert(user + " your funny, I would get a lot more sleep if I didnt!");
   Points++;
 }
 
@@ -52,25 +53,35 @@ switch (car.toLowerCase()) {
     alert("I used to before the children.");
     break;
   case "c63":
-    alert("Nope worked for Mercedes for a decade, I know the problems!");
+    alert("Nope I worked for Mercedes for a decade, I know the problems!");
     break;
   case "type r":
     alert("yes its the perfect family hot hatch, Theo calls it the race car");
     Points++;
     break;
+
   default:
-    alert("what an awful choice!");
+    alert("What even was that?");
 }
-ageQ = prompt(
-  "What is my age, guess a number between 30- 40? You have 4 tries!"
+
+let ageQ = prompt(
+  "What is my age? Guess a number between 30- 40? You have 4 tries!"
 );
-let ageQ = 38;
 let attempts = 3;
+
 while (ageQ != 38 && attempts > 0) {
-  // help* indicate too high or too low?
-  ageQ = prompt("try again");
-  console.log("Correct!");
-  prompt("Well done");
+  // ageQ = prompt("try again " + attempts + " attempts left");
+
+  if (ageQ > 38) {
+    prompt("try a lower number " + attempts + " attempts left");
+  }
+  if (ageQ < 38) {
+    prompt("try a higher number " + attempts + " attempts left");
+  }
+  attempts--;
+}
+if (ageQ === true) {
+  prompt("Nice, you guessed my age");
   Points++;
 }
 alert(user + " I hope you have been paying attention...");
